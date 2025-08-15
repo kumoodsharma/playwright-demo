0c9c2752741da748d8d4eb3b47479b08a85f7177
kumoodsharma <kumoodsharma@gmail.com>
2025-08-15 19:05:03 +1000

    Initial commit

---
+++ pages/LoginPage.ts
@@ -0,0 +1,54 @@
+import { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+  private page: Page;
+  private menuButton: Locator;
+  private emailField: Locator;
+  private continueButton: Locator;
+  private passwordField: Locator;
+  private loginButton: Locator;
+  private userData: Locator;
+  private readonly Logout: Locator
+
+  constructor(page: Page) {
+    this.page = page;
+    this.menuButton = page.getByTestId('HeaderMenu-menu-button');
+    this.emailField = page.locator('input[type="email"]');
+    this.continueButton = page.locator('button:has-text("Continue")');
+    this.passwordField = page.locator('input[type="password"]');
+    this.loginButton = page.locator('button:has-text("Login")');
+    this.userData = page.getByTestId('Menu_userData');
+    this.Logout = page.getByTestId('menu-popper-signout-section')
+  }
+
+  async goto() {
+    await this.page.goto('https://carma.com.au/');
+  }
+
+  async openMenu() {
+    await this.menuButton.click();
+  }
+
+  async enterEmail(email: string) {
+    await this.emailField.click();
+    await this.emailField.fill(email);
+  }
+
+  async clickContinue() {
+    await this.continueButton.click();
+  }
+
+  async enterPassword(password: string) {
+    await this.passwordField.fill(password);
+  }
+
+  async clickLogin() {
+    await this.loginButton.click();
+  }
+
+  async getUserDataLocator() {
+    return this.userData;
+  }
+  async clickLogout() {
+    await this.Logout.click();}
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import { config } from 'dotenv';
+config(); // loads variables from .env
+import { LoginPage } from '../pages/LoginPage';
+
+config(); // loads variables from .env
+
+test('Login to Carma using POM with env variables', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.openMenu();
+  await loginPage.enterEmail(process.env.EMAIL!);
+  await loginPage.clickContinue();
+  await loginPage.enterPassword(process.env.PASSWORD!);
+  await loginPage.clickLogin();
+
+  await loginPage.openMenu();
+  await expect(await loginPage.getUserDataLocator()).toContainText('Hi Kumood');
+
+  await loginPage.clickLogout();
+
+ await loginPage.openMenu();
+ await expect(page.getByTestId('email-visual-input')).toBeVisible();
+
+});
